9d5064095f4dbfc81d814fb9184b1b1042a7ca56
antongeorgescu <antongeorgescu@gmail.com>
2020-04-25 07:01:55 -0400

    Applied a fix

--- idm-frontend-mock/Properties/App.xaml.cs
+++ idm-frontend-mock/Properties/App.xaml.cs
@@ -21,7 +21,9 @@ namespace idm_frontend_mock
             {
                 appSettings = JsonConvert.DeserializeObject<AppSettings>(reader.ReadToEnd());
             }
-
+            ClientId = appSettings.ClientId;
+            Tenant = appSettings.Tenant;
+            Instance = appSettings.Instance;
             PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
                 .WithAuthority($"{Instance}{Tenant}")
                 .WithDefaultRedirectUri()
@@ -37,13 +39,11 @@ namespace idm_frontend_mock
         //   - for any Work or School accounts, use organizations
         //   - for any Work or School accounts, or Microsoft personal account, use e8422127-880e-4288-928e-4ced14423628
         //   - for Microsoft Personal account, use consumers
-        private static string ClientId = appSettings.ClientId;
-
+        public static string ClientId { get; private set; }
+        public static string Tenant { get; private set; }
+        public static string Instance { get; private set; }
         // Note: Tenant is important for the quickstart. We'd need to check with Andre/Portal if we
         // want to change to the AadAuthorityAudience.
-        private static string Tenant = appSettings.Tenant;
-        private static string Instance = appSettings.Instance;
-
         public static IPublicClientApplication PublicClientApp { get; private set; }
     }
 }
